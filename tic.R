af6daa65fbf4a5d7a5bc73cdad12b504693c894d
Patrick Schratz <patrick.schratz@gmail.com>
2020-02-04 18:07:22 +0100

    Enable deployment via Travis (#4)

---
+++ tic.R
@@ -0,0 +1,15 @@
+get_stage("install") %>%
+  add_step(step_install_deps()) %>%
+  add_code_step(blogdown::install_hugo())
+
+get_stage("deploy") %>%
+  add_code_step(blogdown::build_site())
+
+if (ci_can_push() && !ci_is_tag()) {
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+
+  get_stage("deploy") %>%
+    add_step(step_setup_push_deploy(path = "docs", branch = "gh-pages", orphan = TRUE)) %>%
+    add_step(step_do_push_deploy(path = "docs"))
+}
