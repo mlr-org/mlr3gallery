1f2ba892982879997b10cd0c99cd98958153892c
pat-s <patrick.schratz@gmail.com>
2020-02-04 15:10:34 +0100

    deploy

--- tic.R
+++ tic.R
@@ -6,8 +6,8 @@ get_stage("deploy") %>%
   add_code_step(blogdown::build_site())
 
 if (ci_can_push() && !ci_is_tag()) {
-  # get_stage("before_deploy") %>%
-  #   add_step(step_setup_ssh())
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
     add_step(step_push_deploy())
