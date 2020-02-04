a39f652c298c241b62d7da5d9ce187a7e8048aff
pat-s <patrick.schratz@gmail.com>
2020-02-04 15:59:07 +0100

    deploy

--- tic.R
+++ tic.R
@@ -10,5 +10,6 @@ if (ci_can_push() && !ci_is_tag()) {
     add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
-    add_step(step_push_deploy(orphan = TRUE, branch = "gh-pages"))
+    add_step(step_setup_push_deploy(orphan = TRUE, branch = "gh-pages")) %>%
+    add_step(step_push_deploy())
 }
