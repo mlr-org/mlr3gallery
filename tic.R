ac9b81729cf80357204714415f64603adaf56ca9
Marc Becker <marcbecker@posteo.de>
2021-03-08 12:51:23 +0100

    print

--- tic.R
+++ tic.R
@@ -2,9 +2,13 @@ get_stage("install") %>%
   add_step(step_install_deps())
 
 get_stage("script") %>%
+  add_code_step(print(list.files("_posts/",
+    pattern = ".Rmd",
+    full.names = TRUE, recursive = TRUE)) %>%
   add_code_step(lapply(list.files("_posts/",
     pattern = ".Rmd",
     full.names = TRUE, recursive = TRUE), rmarkdown::render))
+
 
 if (ci_get_branch() == "main") {
   get_stage("before_deploy") %>%
