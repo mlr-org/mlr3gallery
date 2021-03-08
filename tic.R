f539da79d870a9ccabc2f931e74b00c5439f65d7
Marc Becker <marcbecker@posteo.de>
2021-03-08 21:40:03 +0100

    files test

--- tic.R
+++ tic.R
@@ -2,9 +2,8 @@ get_stage("install") %>%
   add_step(step_install_deps())
 
 get_stage("script") %>%
-  add_code_step(lapply(list.files("_posts/",
-    pattern = ".Rmd",
-    full.names = TRUE, recursive = TRUE)[1:31], rmarkdown::render))
+  add_code_step({files = list.files("_posts/", pattern = ".Rmd", full.names = TRUE, recursive = TRUE)
+     lapply(files, rmarkdown::render)})
 
 if (ci_get_branch() == "main") {
   get_stage("before_deploy") %>%
