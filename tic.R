9804cf73abdecb5efbdfd6bf9ba5f1af7dba5af7
Marc Becker <marcbecker@posteo.de>
2021-03-08 13:29:10 +0100

    liver

--- tic.R
+++ tic.R
@@ -4,7 +4,7 @@ get_stage("install") %>%
 get_stage("script") %>%
   add_code_step(lapply(list.files("_posts/",
     pattern = ".Rmd",
-    full.names = TRUE, recursive = TRUE)[30], rmarkdown::render))
+    full.names = TRUE, recursive = TRUE)[27], rmarkdown::render))
 
 
 get_stage("before_deploy") %>%
