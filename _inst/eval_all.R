20202d2bcc5837e2104f6dc4a2c334b307d734fe
Ubuntu <marcbecker@posteo.de>
2021-06-13 10:10:39 +0000

    run credit tuning

--- _inst/eval_all.R
+++ _inst/eval_all.R
@@ -1,10 +1,11 @@
 posts = c(
   "2021-03-09-practical-tuning-series-tune-a-support-vector-machine",
   "2021-03-10-practical-tuning-series-tune-a-preprocessing-pipeline",
-  "2021-03-11-practical-tuning-series-build-an-automated-machine-learning-system"
-  "2021-02-03-tuning-a-complex-graph"
-  "2020-01-30-house-prices-in-king-county"
-  "2020-03-11-mlr3pipelines-tutorial-german-credit"
+  "2021-03-11-practical-tuning-series-build-an-automated-machine-learning-system",
+  "2021-02-03-tuning-a-complex-graph",
+  "2020-01-30-house-prices-in-king-county",
+  "2020-03-11-mlr3pipelines-tutorial-german-credit",
+  "2020-03-11-mlr3tuning-tutorial-german-credit"
 )
 
 files = list.files(file.path("./_posts/", posts), pattern = "^.*\\.Rmd", full.names = TRUE)
