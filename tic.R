4812239e5b5209bd7acd2e3ea964cb5907e765e8
Patrick Schratz <patrick.schratz@gmail.com>
2021-11-11 16:58:04 +0100

    Add weekly eval-all workflow (#118)
    * add weekly eval-all wf
    
    * correct cron
    
    * remove rmarkdown 2.10 restriction

--- tic.R
+++ tic.R
@@ -1,32 +1,47 @@
 get_stage("install") %>%
   add_step(step_install_deps())
 
-get_stage("script") %>%
-  add_code_step({
-    remotes::install_version('rmarkdown', version = '2.10', repos = 'http://cran.us.r-project.org') # 2.11 breaks distill
-    files = list.files("_posts/", pattern = ".Rmd", full.names = TRUE, recursive = TRUE)
-    for(f in files) {rmarkdown::render(f)}}) # lapply does not render all posts
-
-# copy static html posts
-get_stage("script") %>%
-  add_code_step({
-    files = list.files("_static", full.names = TRUE)
-    file.copy(files, "_posts", recursive = TRUE)
-  })
-
-if (ci_get_branch() == "main") {
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh()) %>%
-    add_step(step_setup_push_deploy())
-
-  get_stage("deploy") %>%
-    add_code_step(rmarkdown::render_site()) %>%
-    add_code_step(writeLines("mlr3gallery.mlr-org.com", "docs/CNAME")) %>%
-    add_step(step_do_push_deploy(
-      path = ".",
-      commit_paths = "docs/"))
-
+# WEEKLY run which runs all code
+if (ci_get_env("WEEKLY") == "true") {
+  get_stage("script") %>%
+    add_code_step({
+      files = list.files("_posts/", pattern = ".Rmd", full.names = TRUE, recursive = TRUE)
+      for (f in files) {
+        rmarkdown::render(f, encoding = "UTF-8", params = list(eval_all = TRUE))
+      }
+    })
 } else {
-  get_stage("deploy") %>%
-    add_code_step(rmarkdown::render_site())
+
+  get_stage("script") %>%
+    add_code_step({
+      files = list.files("_posts/", pattern = ".Rmd", full.names = TRUE, recursive = TRUE)
+      for (f in files) {
+        rmarkdown::render(f)
+      }
+    })
+
+  # copy static html posts
+  get_stage("script") %>%
+    add_code_step({
+      files = list.files("_static", full.names = TRUE)
+      file.copy(files, "_posts", recursive = TRUE)
+    })
+
+  if (ci_get_branch() == "main") {
+    get_stage("before_deploy") %>%
+      add_step(step_setup_ssh()) %>%
+      add_step(step_setup_push_deploy())
+
+    get_stage("deploy") %>%
+      add_code_step(rmarkdown::render_site()) %>%
+      add_code_step(writeLines("mlr3gallery.mlr-org.com", "docs/CNAME")) %>%
+      add_step(step_do_push_deploy(
+        path = ".",
+        commit_paths = "docs/"))
+
+  } else {
+    get_stage("deploy") %>%
+      add_code_step(rmarkdown::render_site())
+  }
+
 }
